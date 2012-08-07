27fed331c49d2aabad2c7a282bf41fa5e2c8fe9e
Casey Hunt <casey.hunt@gmail.com>
2012-08-07 16:09:48 -0500

    tabs working, IE displays

--- sitescripts/search-box/searchbox-scout-form.php
+++ sitescripts/search-box/searchbox-scout-form.php
@@ -9,6 +9,7 @@ else {
 $query = "site";
 }
 
+// --- All Tab ---
 if ($query=="all"){
 echo '
 			<div id="searchcontent" class="gradient">
@@ -31,7 +32,7 @@ echo '
 				</div>
 				<div id="searchbottom">
 					<ul id="searchbottom-links">
-						<li><a href="#">scoUT Mobile</a></li>
+						<li><a href="http://utexas.summon.serialssolutions.com/">scoUT Mobile</a></li>
 						<li><a href="#">Search Tips</a></li>
 						<li><a href="#">Feedback</a></li>
 					</ul>	
@@ -41,6 +42,7 @@ echo '
 ';
 }
 
+// --- Articles Tab ---
 else if ($query=="articles"){
 echo '
 			<div id="searchcontent" class="gradient">
@@ -66,87 +68,146 @@ echo '
 				</div>
 				<div id="searchbottom">
 					<ul id="searchbottom-links">
-						<li style="float: left;"><a href="#">scoUT Mobile</a></li>
-						<li style="float: right;"><a href="#">Google Scholar</a></li>
+						<li style="float: left;"><a href="http://utexas.summon.serialssolutions.com/">scoUT Mobile</a></li>
+						<li style="float: right;"><a href="http://scholar.google.com/">Google Scholar</a></li>
 					</ul>	
 					<div style="clear:both;"></div>
 				</div>
 			</div>
 ';
 }
-else if ($query=="journal"){
+
+// --- Catalog Tab ---
+else if ($query=="catalog"){
 echo '
-<form action="/sitescripts/search-box/searchbox.php" method="get">
-        <label for="query" id="querylabel">JOURNALS</label>
-        <input type="hidden" id="engine" name="engine" value="journals" />
-        <input type="text" name="query" id="query" title="" value="" />
-        <input type="image" src="/graphics/search-box/button-search-up.png" alt="Find" class="find" value="Find" />
-        <br />
-        <input type="radio" name="subengine" id="electjournal" value="ejournal" checked="checked" />
-        <label for="ejournal" style="display:inline;">Electronic Journals</label>
-        <input type="radio" name="subengine" id="printjournal" value="pjournal" />
-        <label for="pjournal" style="display:inline;">Print Journals</label>
-</form>
-';
-}
-else if ($query=="database"){
-echo '
-<form action="http://www.lib.utexas.edu/indexes/results.html" 
-method="get">
-        <label for="query" id="querylabel">DATABASES</label>
-        <input type="hidden" id="engine" name="engine" value="metalib" />
-        <input type="text" name="query" id="query" title="" value="" />
-        <input type="image" src="/graphics/search-box/button-search-up.png" alt="Find" class="find" value="Find" />
-</form>
-<p style="font-size:10px;line-height:9px;">Browse:
-
-<a href="/indexes/titles.html?let=A">A</a>
-
- <a href="/indexes/titles.html?let=B">B</a>
-
- <a href="/indexes/titles.html?let=C">C</a>
-
- <a href="/indexes/titles.html?let=D">D</a>
-
- <a href="/indexes/titles.html?let=E">E</a>
-
- <a href="/indexes/titles.html?let=F">F</a>
-
- <a href="/indexes/titles.html?let=G">G</a>
-
- <a href="/indexes/titles.html?let=H">H</a>
-
- <a href="/indexes/titles.html?let=I">I</a>
-
- <a href="/indexes/titles.html?let=J">J</a>
-
- <a href="/indexes/titles.html?let=K">K</a>
-
- <a href="/indexes/titles.html?let=L">L</a>
-
- <a href="/indexes/titles.html?let=M">M</a>
-
- <a href="/indexes/titles.html?let=N">N</a>
-
- <a href="/indexes/titles.html?let=O">O</a>
-
- <a href="/indexes/titles.html?let=P">P</a> Q
-
- <a href="/indexes/titles.html?let=R">R</a>
-
- <a href="/indexes/titles.html?let=S">S</a>
-
- <a href="/indexes/titles.html?let=T">T</a>
-
- <a href="/indexes/titles.html?let=U">U</a>
-
- <a href="/indexes/titles.html?let=V">V</a>
-
- <a href="/indexes/titles.html?let=W">W</a> X Y Z
-</p>
+				<div id="searchcontent" class="gradient">
+					<div id="searchtop">
+						<div style="margin-top:1em;">
+							<form>
+								<p style="display: inline;">Search Catalog for</p>
+								<input type="textfield" size="25" name="query" id="query">
+								<select name="subengine">
+									<option value="key-phrase">keyword or phrase</option>
+									<option value="author">author</option>
+									<option value="title">title</option>
+									<option value="journal">journal title</option>
+									<option value="isbn-issn">ISBN/ISSN</option>
+									<option value="oclc">OCLC</option>
+								</select>
+								<input class="findbutton" type="button" value="find"></input>
+								<p style="margin-top: 0; margin-left: 10.25em; float: left;">Books, journals, DVDs and more</p>
+								<p style="float: right; margin-top: 0; clear: right;"><a href="#">Advanced Search</a></p>
+								<div style="clear:both;"></div>
+								<table width="100%">
+									<tr>
+										<td>
+											<input type="checkbox" name="format" value="any" class="checkbox" checked="yes">Any format</input>
+										</td>
+										<td>
+											<input type="checkbox" name="format" value="book" class="checkbox">Books, eBooks</input>
+										</td>
+										<td>
+											<input type="checkbox" name="format" value="smallprint" class="checkbox">Journals, newspapers, magazines</input>
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<input type="checkbox" name="format" value="dvd" class="checkbox">DVDs</input>
+										</td>
+										<td>
+											<input type="checkbox" name="format" value="cd" class="checkbox">CDs</input>
+										</td>
+										<td>
+											<input type="checkbox" name="format" value="map" class="checkbox">Maps</input>
+										</td>
+								</table>
+							</form>
+						</div>
+					</div>
+					
+					<div id="searchbottom">
+						<ul id="searchbottom-links">
+							<li><a href="#">scoUT Mobile</a></li>
+							<li><a href="#">Search Tips</a></li>
+							<li><a href="#">Feedback</a></li>
+						</ul>	
+						<div style="clear:both;"></div>
+					</div>
+				</div>
 ';
 }
 
+// --- Databases Tab ---
+else if ($query=="databases"){
+echo '
+				<div id="searchcontent" class="gradient">
+					<div id="searchtop">
+						<div style="margin-top:1em;">
+							<form>
+								<input type="textfield" size="65" name="query" id="query">
+								<input class="findbutton" type="button" value="find"></input>
+								<p style="margin-top: 0; float: left; margin-left: 5px;">Find databases by name or subject</p>
+								<p style="float: right; margin-top: 0; clear: right;"><a href="#">Advanced Search</a></p>
+								
+								<p style="margin-top: 0; float: left; margin-left: 5px;">Recommended Databases: <a href="#">Academic Search Complete</a>, <a href="#">JSTOR</a>, <a href="#">PubMed</a></p>
+								<div style="clear:both;"></div>
+							</form>
+						</div>
+					</div>
+					
+					<div id="searchbottom">
+						<p class="moredatabases">
+							<a href="/indexes/titles.php?let=0-9">0-9</a>
+							<a href="/indexes/titles.html?let=2">2</a>
+							<a href="/indexes/titles.html?let=A">A</a>
+							<a href="/indexes/titles.html?let=B">B</a>
+							<a href="/indexes/titles.html?let=C">C</a>
+							<a href="/indexes/titles.html?let=D">D</a>
+							<a href="/indexes/titles.html?let=E">E</a>
+							<a href="/indexes/titles.html?let=F">F</a>
+							<a href="/indexes/titles.html?let=G">G</a>
+							<a href="/indexes/titles.html?let=H">H</a>
+							<a href="/indexes/titles.html?let=I">I</a>
+							<a href="/indexes/titles.html?let=J">J</a>
+							<a href="/indexes/titles.html?let=K">K</a>
+							<a href="/indexes/titles.html?let=L">L</a>
+							<a href="/indexes/titles.html?let=M">M</a>
+							<a href="/indexes/titles.html?let=N">N</a>
+							<a href="/indexes/titles.html?let=O">O</a>
+							<a href="/indexes/titles.html?let=P">P</a>
+							<a href="/indexes/titles.html?let=R">R</a>
+							<a href="/indexes/titles.html?let=S">S</a>
+							<a href="/indexes/titles.html?let=T">T</a>
+							<a href="/indexes/titles.html?let=U">U</a>
+							<a href="/indexes/titles.html?let=V">V</a>
+							<a href="/indexes/titles.html?let=W">W</a>
+						</p>
+						<p class="moredatabases2">
+							<a href="/indexes/titles.php?let=[A-Z]">View all databases</a>
+							<a href="/indexes/subjectpopup.php">Browse by subject</a>
+							<a href="/indexes/typepopup.php">Browse by type</a>
+						</p>
+						<div style="clear:both;"></div>
+					</div>
+				</div>
+';
+}
+
+// --- Journals Tab ---
+else if ($query=="journals"){
+echo '
+				
+';
+}
+
+// --- Site Search Tab ---
+else if ($query=="site"){
+echo '
+				
+';
+}
+
+// --- Contingency plan ---
 else {
 echo '
 			<div id="searchcontent" class="gradient">
--- sitescripts/search-box/searchbox-scout-formdefault.php
+++ sitescripts/search-box/searchbox-scout-formdefault.php
@@ -4,28 +4,28 @@
 		<div id="leftside">
 			<nav id="searchtabs">
 				<ul class="text-stroke-white">
-					<li id="all" class="active"><a href="/index-scout.html?searchtype=all">All</a></li>
-					<li id="articles" class="inactive"><a href="/index-scout.html?searchtype=articles">Articles</a></li>
-					<li id="catalog" class="inactive"><a href="#">Catalog</a></li>
-					<li class="inactive"><a href="#">Databases</a></li>
-					<li class="inactive"><a href="#">Journals</a></li>
-					<li class="inactive"><a href="#">Site Search</a></li>
+					<li id="all" class="active"><a href="/index-scout.html?searchtype=all" id="all">All</a></li>
+					<li id="articles" class="inactive"><a href="/index-scout.html?searchtype=articles" id="articles">Articles</a></li>
+					<li id="catalog" class="inactive"><a href="/index-scout.html?searchtype=catalog" id="catalog">Catalog</a></li>
+					<li id="databases" class="inactive"><a href="/index-scout.html?searchtype=databases" id="databases">Databases</a></li>
+					<li id="journals" class="inactive"><a href="/index-scout.html?searchtype=journals" id="journals">Journals</a></li>
+					<li id="site" class="inactive"><a href="/index-scout.html?searchtype=site" id="site">Site Search</a></li>
 				</ul>
 			</nav>
-			
-			<?php 
-			if (isset($_GET[searchtype]))
-			{
-				$searchtype = $_GET[searchtype];
-				include('sitescripts/search-box-scout/searchbox-scout-form.php');
-			}
+				<div id="bazinga">
+					<?php 
+					if (isset($_GET[searchtype]))
+					{
+						$searchtype = $_GET[searchtype];
+						include('sitescripts/search-box-scout/searchbox-scout-form.php');
+					}
 
-			else { 
-				$searchtype = "all";
-				include('sitescripts/search-box-scout/searchbox-scout-form.php');
-			}
-			?>
-			
+					else { 
+						$searchtype = "all";
+						include('sitescripts/search-box-scout/searchbox-scout-form.php');
+					}
+					?>
+				</div>
 		</div>
 	</div>
 </div>
